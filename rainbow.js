d52303b2020801d4ee57dead5cf0935f5e8a7234
BaluSharma <balanjaneyasarma@gmail.com>
2023-05-08 21:44:40 +0530

    initial commit

---
+++ rainbow.js
@@ -0,0 +1,10 @@
+sum=(m)=>{
+    let count =0;
+    for(let i = 1; i<m;i++){
+        if(i%3==0 && i%5==0){
+            count+=i;
+        }
+    }
+    return count;
+}
+console.log(sum(345676565));
